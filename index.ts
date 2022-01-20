ba4e723a9dd2d06c73804e3c40f0004ddda1dc87
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 10:49:40 +0100

    init

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from "apollo-server";
+import Mutation from "./resolvers/mutation";
+import Query from "./resolvers/query";
+import typeDefs from "./schema";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Mutation,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
