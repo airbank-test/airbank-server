105542985ccdb00080da84d8833a8ebb32e0ba02
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 10:45:26 +0100

    generated migrations

---
+++ prisma/migrations/20220120093152_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "account" VARCHAR(255),
+    "description" VARCHAR(255),
+    "category" VARCHAR(255),
+    "reference" VARCHAR(255),
+    "currency" VARCHAR(255),
+    "amount" DOUBLE PRECISION NOT NULL,
+    "status" VARCHAR(255),
+    "transactionDate" TEXT NOT NULL,
+    "createdAt" TEXT NOT NULL,
+    "updatedAt" TEXT NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
