23cbb6adf62c99d1410071faba4b526f92ea8b15
philipszdavido <kurtwanger40@gmail.com>
2022-01-21 14:18:33 +0100

    fix

---
+++ schema/schema.ts
@@ -0,0 +1,39 @@
+import { gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Transaction {
+    id: ID!
+    account: String!
+    description: String!
+    category: String
+    reference: String
+    currency: String
+    amount: String
+    status: String
+    transactionDate: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Query {
+    transactions: [Transaction]
+    transaction(id: Int!): Transaction
+  }
+
+  type Mutation {
+    createTransaction(
+      account: String!
+      description: String!
+      category: String
+      reference: String
+      currency: String
+      amount: String
+      status: String
+      transactionDate: String
+      createdAt: String
+      updatedAt: String
+    ): Transaction
+  }
+`;
+
+export default typeDefs;
